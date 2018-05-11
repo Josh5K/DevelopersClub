0c8f8dbed75f666e179df985d6b9a4538fac95a6
Josh5K <jking57@academic.rrc.ca>
2018-05-10 21:15:10 -0500

    Added Navbar, Landing, and Footer modules.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
