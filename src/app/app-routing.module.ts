512735c3803a298a7ca20c10bfa23bab224bf1e4
Josh5K <jking57@academic.rrc.ca>
2018-05-14 20:32:47 -0500

    sign-up/login WIP commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,16 @@ import { RouterModule, Routes, Router } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ProjectComponent } from './project/project.component';
+import { LoginComponent } from "./login/login.component";
+import { SignUpComponent } from "./sign-up/sign-up.component";
 
 const routes: Routes = [
   //Paths
   { path: 'home', component: LandingComponent },
   { path: '404', component: NotFoundComponent },
   { path: 'project', component: ProjectComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'sign-up', component: SignUpComponent },
   //Redirects
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', redirectTo: '/404' },
