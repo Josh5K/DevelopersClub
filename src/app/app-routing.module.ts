7ee03c21ddd73a23b3c918de03f126395d45d7ff
Josh King <jking57@academic.rrc.ca>
2018-07-07 01:45:37 -0500

    Added User Profile Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,17 +9,19 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { ProjectComponent } from './project/project.component';
 import { LoginComponent } from "./login/login.component";
 import { SignUpComponent } from "./sign-up/sign-up.component";
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   //Paths
   { path: 'home', component: LandingComponent },
+  { path: 'Profile/:profile', component: ProfileComponent },
   { path: '404', component: NotFoundComponent },
   { path: 'project', component: ProjectComponent },
   { path: 'login', component: LoginComponent },
   { path: 'sign-up', component: SignUpComponent },
   //Redirects
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: '**', redirectTo: '/404' },
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent },
 ]
 
 @NgModule({
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  username;
+  constructor(private route: ActivatedRoute, private http: HttpClient) { 
+    this.username = this.route.snapshot.params.profile;
   }
+  user;
+  projects;
+  userid;
+  ngOnInit() {
+    var headers = new HttpHeaders().set('name', this.username);
+    
+    this.http.get('http://localhost:3000/users', {
+      headers: headers
+    }).subscribe(data => {
+      this.user = data;
+      this.userid = data[0].id;
+      console.log(this.userid);
 
+      headers = new HttpHeaders().set('id', this.userid);
+      this.http.get('http://localhost:3000/users/projects', {
+        headers: headers
+      }).subscribe(data => {
+        this.projects = data;
+        console.log(data);
+      });
+    });
+    
+
+  }
 }
