0fd27cd70750723b485bf14ddab13d38e65a01f9
Josh5K <jking@academic.rrc.ca>
2018-08-08 22:38:23 -0700

    Headline changes

--- src/app/components/headline/headline.component.ts
+++ src/app/components/headline/headline.component.ts
@@ -7,11 +7,19 @@ import { LandingComponent } from '../../pages/landing/landing.component';
   templateUrl: './headline.component.html',
   styleUrls: ['./headline.component.css']
 })
+
 export class HeadlineComponent implements OnInit {
+
+  public current: String[];
+  //Temp values will be pulled from API
+  public tab1: String[] = ["tab1","tab1","tab1","tab1","tab1","tab1","tab1","tab1","tab1","tab1"];
+  public tab2: String[] = ["tab2","tab2","tab2","tab2","tab2","tab2","tab2","tab2","tab2","tab2"];
+  public tab3: String[] = ["tab3","tab3","tab3","tab3","tab3","tab3","tab3","tab3","tab3","tab3"];
 
   constructor() { }
 
   ngOnInit() {
+    this.current = this.tab1;
   }
 
 }
